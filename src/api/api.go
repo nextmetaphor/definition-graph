b37b70ce383b36e95d5628cf6bbc9348d568519f
nextmetaphor <paul@nextmetaphor.com>
2024-02-28 22:20:13 +0000

    temporary CORS hack for testing

--- src/api/api.go
+++ src/api/api.go
@@ -27,6 +27,11 @@ func nodeClassGraphHandler(w http.ResponseWriter, r *http.Request) {
 
 func nodeGraphHandler(w http.ResponseWriter, r *http.Request) {
 	b, err := core.SelectNodeGraph(db)
+
+	//TODO - sort this out
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 	} else {
